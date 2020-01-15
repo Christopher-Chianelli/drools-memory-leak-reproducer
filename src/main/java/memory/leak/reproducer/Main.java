77bf7bdf1aac758cb51f6326748e75d823ecd6c4
Christopher Chianelli <chris.chianelli@gmail.com>
2020-01-15 12:59:05 -0500

    Read the MOVE_LIST and perform the rules (no OOM here...)

--- src/main/java/memory/leak/reproducer/Main.java
+++ src/main/java/memory/leak/reproducer/Main.java
@@ -1,11 +1,13 @@
 package memory.leak.reproducer;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,6 +20,13 @@ import memory.leak.reproducer.domain.employee.Employee;
 import memory.leak.reproducer.domain.roster.Roster;
 import memory.leak.reproducer.domain.shift.Shift;
 import memory.leak.reproducer.generator.RosterGenerator;
+import org.apache.commons.lang3.tuple.Pair;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.optaplanner.core.api.score.holder.ScoreHolder;
+import org.optaplanner.core.impl.score.buildin.hardmediumsoftlong.HardMediumSoftLongScoreDefinition;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,6 +38,24 @@ public class Main {
 	
 	roster.getShiftList().forEach(shift -> shiftIdToShiftMap.put(shift.getId(), shift));
 	roster.getEmployeeList().forEach(employee -> employeeIdToEmployeeMap.put(employee.getId(), employee));
+
+    KieServices kieServices = KieServices.Factory.get();
+    KieFileSystem kfs = kieServices.newKieFileSystem();
+    kfs.write(kieServices.getResources()
+                .newFileSystemResource(new File("src/main/resources/memory/leak/reproducer/service/solver/employeeRosteringScoreRules.drl"), "UTF-8"));
+    kieServices.newKieBuilder(kfs).buildAll();
+    KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+    KieSession kieSession = kieContainer.newKieSession();
+
+    ScoreHolder scoreHolder = new HardMediumSoftLongScoreDefinition().buildScoreHolder(true);
+    kieSession.setGlobal("scoreHolder", scoreHolder);
+    
+    roster.getShiftList().forEach(shift -> kieSession.insert(shift));
+    roster.getEmployeeAvailabilityList().forEach(ea -> kieSession.insert(ea));
+    roster.getEmployeeList().forEach(e -> kieSession.insert(e));
+    roster.getSkillList().forEach(s -> kieSession.insert(s));
+    roster.getSpotList().forEach(s -> kieSession.insert(s));
+    
     
     try (BufferedReader reader = Files.newBufferedReader(Paths.get("MOVE_LIST.txt"))) {
         String line;
@@ -44,9 +71,21 @@ public class Main {
             }
             else {
                 if (readType.isEnd(READ_LINE)) {
-                    Roster move = dataHandler.getMove();
+                    List<Pair<Shift, Employee>> move = dataHandler.getMove();
+                    List<Pair<Shift, Employee>> undoMove = dataHandler.getUndoMove();
                     // Do the drools thing
-                    roster = dataHandler.getNewBase();
+                    move.forEach(pair -> {
+                        pair.getLeft().setEmployee(pair.getRight());
+                        kieSession.update(kieSession.getFactHandle(pair.getLeft()), pair.getLeft(), "employee");
+                    });
+                    if (!undoMove.isEmpty()) {
+                        kieSession.fireAllRules();
+                        undoMove.forEach(pair -> {
+                            pair.getLeft().setEmployee(pair.getRight());
+                            kieSession.update(kieSession.getFactHandle(pair.getLeft()), pair.getLeft(), "employee");
+                        });
+                    }
+                    readType = null;
                 }
                 else {
                     dataHandler.handleLine(READ_LINE);
@@ -89,8 +128,8 @@ public class Main {
     interface DataHandler {
         void init(Roster roster, Map<Long, Shift> shiftIdToShiftMap, Map<Long, Employee> employeeIdToEmployeeMap);
         void handleLine(String line);
-        Roster getMove();
-        Roster getNewBase();
+        List<Pair<Shift, Employee>> getMove();
+        List<Pair<Shift, Employee>> getUndoMove();
     }
     
     static class MoveDataHandler implements DataHandler {
@@ -122,25 +161,15 @@ public class Main {
         }
         
         @Override
-        public Roster getMove() {
-            Set<Shift> affectedShiftSet = new HashSet<>(affectedShifts);
-            List<Shift> newShiftList = new ArrayList<>(originalRoster.getShiftList().stream().filter(s -> !affectedShiftSet.contains(s)).collect(Collectors.toList()));
-            affectedShifts.forEach(shift -> {
-                Shift newShift = new Shift(shift.getTenantId(), shift.getSpot(), shift.getStartDateTime(), shift.getEndDateTime(),
-                                           shift.getRotationEmployee());
-                newShift.setId(shift.getId());
-                newShift.setEmployee(moveTo);
-                newShiftList.add(newShift);
-            });
-            return new Roster(originalRoster.getId(), originalRoster.getTenantId(), originalRoster.getSkillList(), originalRoster.getSpotList(), originalRoster.getEmployeeList(),
-                                       originalRoster.getEmployeeAvailabilityList(), originalRoster.getRosterParametrization(),
-                                       originalRoster.getRosterState(), newShiftList);
+        public List<Pair<Shift, Employee>> getMove() {
+            return affectedShifts.stream().map(s -> Pair.of(s, moveTo)).collect(Collectors.toList());
         }
         
         @Override
-        public Roster getNewBase() {
-            return originalRoster;
-        }   
+        public List<Pair<Shift, Employee>> getUndoMove() {
+            return affectedShifts.stream().map(s -> Pair.of(s, s.getEmployee())).collect(Collectors.toList());
+        }
+        
     }
     
     static class SolutionDataHandler implements DataHandler {
@@ -149,11 +178,13 @@ public class Main {
         List<Shift> shiftList;
         int shiftIndex;
         Map<Long, Employee> employeeIdToEmployeeMap;
+        Map<Long, Employee> shiftIdToEmployeeMap;
         
         @Override
         public void init(Roster roster, Map<Long, Shift> shiftIdToShiftMap, Map<Long, Employee> employeeIdToEmployeeMap) {
             this.roster = roster;
             this.employeeIdToEmployeeMap = employeeIdToEmployeeMap;
+            shiftIdToEmployeeMap = new HashMap<>();
             shiftIndex = 0;
             shiftList = new ArrayList<>(roster.getShiftList().size());
         }
@@ -162,20 +193,19 @@ public class Main {
         public void handleLine(String line) {
             Employee employee = (line.equals("null"))? null : employeeIdToEmployeeMap.get(Long.valueOf(line));
             Shift shift = roster.getShiftList().get(shiftIndex);
-            shift.setEmployee(employee);
+            shiftIdToEmployeeMap.put(shift.getId(), employee);
             shiftList.add(shift);
+            shiftIndex++;
         }
         
         @Override
-        public Roster getMove() {
-            roster.setShiftList(shiftList);
-            return roster;
+        public List<Pair<Shift, Employee>> getMove() {
+            return shiftList.stream().map(s -> Pair.of(s, shiftIdToEmployeeMap.get(s.getId()))).collect(Collectors.toList());
         }
         
-        @Override
-        public Roster getNewBase() {
-            roster.setShiftList(shiftList);
-            return roster;
+        public List<Pair<Shift, Employee>> getUndoMove() {
+            return Collections.emptyList();
         }
+        
     }
 }
